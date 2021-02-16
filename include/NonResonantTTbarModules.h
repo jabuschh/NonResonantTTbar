23cf9c0c7e9acbdf8b63c126937c706744574d7f
Henrik Jabusch <henrik.jabusch@uni-hamburg.de>
2021-02-16 18:50:33 +0100

    setup preselection from Ksenia's ZPrime analysis

---
+++ include/NonResonantTTbarModules.h
@@ -0,0 +1,172 @@
+#pragma once
+
+#include <UHH2/core/include/AnalysisModule.h>
+#include <UHH2/core/include/Event.h>
+#include <UHH2/core/include/NtupleObjects.h>
+#include <UHH2/core/include/LorentzVector.h>
+#include <UHH2/common/include/TTbarGen.h>
+#include <UHH2/NonResonantTTbar/include/TTbarCandidate.h>
+
+#include "TH1.h"
+
+float inv_mass(const LorentzVector&);
+
+class TTbarCandidateBuilder : uhh2::AnalysisModule{
+
+public:
+  explicit TTbarCandidateBuilder(uhh2::Context&, TString mode, float minDR = 1.2);
+  virtual bool process(uhh2::Event&) override;
+
+private:
+  uhh2::Event::Handle< std::vector<TTbarCandidate> > h_TTbarCandidates_;
+  uhh2::Event::Handle< std::vector<TopJet> > h_AK8TopTags;
+  uhh2::Event::Handle< std::vector<const TopJet*> > h_AK8TopTagsPtr;
+
+  float minDR_;
+  TString mode_;
+
+};
+
+class TTbarChi2Discriminator : uhh2::AnalysisModule{
+
+public:
+  explicit TTbarChi2Discriminator(uhh2::Context&);
+  virtual bool process(uhh2::Event&) override;
+
+private:
+  uhh2::Event::Handle< std::vector<TTbarCandidate> > h_TTbarCandidates_;
+  uhh2::Event::Handle<TTbarCandidate*> h_BestCandidate_;
+  uhh2::Event::Handle<bool> h_is_zprime_reconstructed_;
+  float mtoplep_, mtoplep_ttag_;
+  float sigmatoplep_, sigmatoplep_ttag_;
+  float mtophad_, mtophad_ttag_;
+  float sigmatophad_, sigmatophad_ttag_;
+
+};
+
+class TTbarCorrectMatchDiscriminator : uhh2::AnalysisModule{
+
+public:
+  explicit TTbarCorrectMatchDiscriminator(uhh2::Context&);
+  virtual bool process(uhh2::Event&) override;
+
+private:
+  uhh2::Event::Handle< std::vector<TTbarCandidate> > h_TTbarCandidates_;
+  uhh2::Event::Handle<TTbarGen> h_ttbargen_;
+  uhh2::Event::Handle<TTbarCandidate*> h_BestCandidate_;
+  uhh2::Event::Handle<bool> h_is_zprime_reconstructed_;
+
+  bool is_mc;
+  std::unique_ptr<TTbarGenProducer> ttgenprod;
+
+};
+
+class AK8PuppiTopTagger : public uhh2::AnalysisModule {
+
+public:
+  explicit AK8PuppiTopTagger(uhh2::Context&, int min_num_daughters = 2, float max_dR = 1., float min_mass = 105., float max_mass = 210., float max_tau32 = 0.65);
+  virtual bool process(uhh2::Event&) override;
+
+private:
+  int min_num_daughters_ = 2;
+  float max_dR_;
+  float min_mass_;
+  float max_mass_;
+  float max_tau32_;
+  uhh2::Event::Handle< std::vector<TopJet> > h_AK8PuppiTopTags_;
+  uhh2::Event::Handle< std::vector<const TopJet*> > h_AK8PuppiTopTagsPtr_;
+};
+
+
+
+
+class JetLeptonDeltaRCleaner : public uhh2::AnalysisModule {
+
+public:
+  explicit JetLeptonDeltaRCleaner(float mindr=0.8): minDR_(mindr) {}
+  virtual bool process(uhh2::Event&) override;
+
+private:
+  float minDR_;
+};
+////
+
+class TopJetLeptonDeltaRCleaner : public uhh2::AnalysisModule {
+
+public:
+  explicit TopJetLeptonDeltaRCleaner(float mindr=0.8): minDR_(mindr) {}
+  virtual bool process(uhh2::Event&) override;
+
+private:
+  float minDR_;
+};
+////
+
+const Particle* leading_lepton(const uhh2::Event&);
+
+float STlep(const uhh2::Event&);
+
+float Muon_pfMINIIso    (const Muon&    , const uhh2::Event&, const std::string&);
+float Electron_pfMINIIso(const Electron&, const uhh2::Event&, const std::string&);
+////
+
+bool trigger_bit(const uhh2::Event&, const std::string&);
+////
+
+class GENWToLNuFinder : public uhh2::AnalysisModule {
+
+public:
+  explicit GENWToLNuFinder(uhh2::Context&, const std::string&);
+  virtual ~GENWToLNuFinder() {}
+
+  virtual bool process(uhh2::Event&) override;
+
+protected:
+  uhh2::Event::Handle<GenParticle> h_genWln_W_;
+  uhh2::Event::Handle<GenParticle> h_genWln_l_;
+  uhh2::Event::Handle<GenParticle> h_genWln_n_;
+};
+
+class MEPartonFinder : public uhh2::AnalysisModule {
+
+public:
+  explicit MEPartonFinder(uhh2::Context&, const std::string&);
+  virtual ~MEPartonFinder() {}
+
+  virtual bool process(uhh2::Event&) override;
+
+protected:
+  uhh2::Event::Handle<std::vector<GenParticle> > h_meps_;
+};
+
+
+////////////////////////    NN vars
+class Variables_NN : uhh2::AnalysisModule{
+
+public:
+  explicit Variables_NN(uhh2::Context&);
+  virtual bool process(uhh2::Event&) override;
+
+private:
+  uhh2::Event::Handle<bool> h_is_zprime_reconstructed_chi2;
+  uhh2::Event::Handle<TTbarCandidate*> h_BestTTbarCandidateChi2;
+  uhh2::Event::Handle< float > h_eventweight;
+  uhh2::Event::Handle< float > h_Mu_pt, h_Mu_eta, h_Mu_phi, h_Mu_E;
+  uhh2::Event::Handle< float > h_Ele_pt, h_Ele_eta, h_Ele_phi, h_Ele_E;
+  uhh2::Event::Handle< float > h_MET_pt, h_MET_phi;
+  uhh2::Event::Handle< float > h_N_Ak8;
+  uhh2::Event::Handle< float > h_Ak8_j1_pt, h_Ak8_j1_eta, h_Ak8_j1_phi, h_Ak8_j1_E, h_Ak8_j1_mSD, h_Ak8_j1_tau21, h_Ak8_j1_tau32;
+  uhh2::Event::Handle< float > h_Ak8_j2_pt, h_Ak8_j2_eta, h_Ak8_j2_phi, h_Ak8_j2_E, h_Ak8_j2_mSD, h_Ak8_j2_tau21, h_Ak8_j2_tau32;
+  uhh2::Event::Handle< float > h_Ak8_j3_pt, h_Ak8_j3_eta, h_Ak8_j3_phi, h_Ak8_j3_E, h_Ak8_j3_mSD, h_Ak8_j3_tau21, h_Ak8_j3_tau32;
+  uhh2::Event::Handle< float > h_N_Ak4;
+  uhh2::Event::Handle< float > h_Ak4_j1_pt, h_Ak4_j1_eta, h_Ak4_j1_phi, h_Ak4_j1_E, h_Ak4_j1_m, h_Ak4_j1_deepjetbscore;
+  uhh2::Event::Handle< float > h_Ak4_j2_pt, h_Ak4_j2_eta, h_Ak4_j2_phi, h_Ak4_j2_E, h_Ak4_j2_m, h_Ak4_j2_deepjetbscore;
+  uhh2::Event::Handle< float > h_Ak4_j3_pt, h_Ak4_j3_eta, h_Ak4_j3_phi, h_Ak4_j3_E, h_Ak4_j3_m, h_Ak4_j3_deepjetbscore;
+  uhh2::Event::Handle< float > h_Ak4_j4_pt, h_Ak4_j4_eta, h_Ak4_j4_phi, h_Ak4_j4_E, h_Ak4_j4_m, h_Ak4_j4_deepjetbscore;
+  uhh2::Event::Handle< float > h_Ak4_j5_pt, h_Ak4_j5_eta, h_Ak4_j5_phi, h_Ak4_j5_E, h_Ak4_j5_m, h_Ak4_j5_deepjetbscore;
+  uhh2::Event::Handle< float > h_Ak4_j6_pt, h_Ak4_j6_eta, h_Ak4_j6_phi, h_Ak4_j6_E, h_Ak4_j6_m, h_Ak4_j6_deepjetbscore;
+  uhh2::Event::Handle< float > h_M_tt;
+
+};
+
+////
---
+++ src/NonResonantTTbarPreselectionModule.cxx
@@ -0,0 +1,326 @@
+#include <iostream>
+#include <memory>
+
+#include <UHH2/core/include/AnalysisModule.h>
+#include <UHH2/core/include/Event.h>
+#include <UHH2/core/include/Selection.h>
+#include "UHH2/common/include/PrintingModules.h"
+
+#include <UHH2/common/include/CleaningModules.h>
+#include <UHH2/common/include/NSelections.h>
+#include <UHH2/common/include/LumiSelection.h>
+#include <UHH2/common/include/TriggerSelection.h>
+#include <UHH2/common/include/JetCorrections.h>
+#include <UHH2/common/include/JetCorrectionSets.h>
+#include <UHH2/common/include/ObjectIdUtils.h>
+#include <UHH2/common/include/MuonIds.h>
+#include <UHH2/common/include/ElectronIds.h>
+#include <UHH2/common/include/JetIds.h>
+#include <UHH2/common/include/TopJetIds.h>
+#include <UHH2/common/include/TTbarGen.h>
+#include <UHH2/common/include/Utils.h>
+#include <UHH2/common/include/AdditionalSelections.h>
+#include "UHH2/common/include/LuminosityHists.h"
+#include <UHH2/common/include/MuonHists.h>
+#include <UHH2/common/include/ElectronHists.h>
+#include <UHH2/common/include/JetHists.h>
+#include <UHH2/common/include/EventHists.h>
+#include <UHH2/common/include/CommonModules.h>
+
+#include <UHH2/NonResonantTTbar/include/ModuleBASE.h>
+// #include <UHH2/NonResonantTTbar/include/NonResonantTTbarSelections.h>
+// #include <UHH2/NonResonantTTbar/include/NonResonantTTbarModules.h>
+#include <UHH2/NonResonantTTbar/include/NonResonantTTbarPreselectionHists.h>
+#include <UHH2/NonResonantTTbar/include/NonResonantTTbarGeneratorHists.h>
+#include <UHH2/NonResonantTTbar/include/TopJetCorrections.h>
+#include <UHH2/NonResonantTTbar/include/TopPuppiJetCorrections.h>
+
+using namespace std;
+
+class NonResonantTTbarPreselectionModule : public ModuleBASE {
+
+public:
+  explicit NonResonantTTbarPreselectionModule(uhh2::Context&);
+  virtual bool process(uhh2::Event&) override;
+  void book_histograms(uhh2::Context&, vector<string>);
+  void fill_histograms(uhh2::Event&, string);
+
+protected:
+
+
+  // Corrections
+  std::unique_ptr<CommonModules>   common;
+  std::unique_ptr<TopJetCorrections> topjetCorr;
+  std::unique_ptr<TopPuppiJetCorrections> toppuppijetCorr;
+
+  // Cleaners
+  std::unique_ptr<JetCleaner>                      jet_IDcleaner, jet_cleaner1, jet_cleaner2;
+  std::unique_ptr<TopJetCleaner>                   topjet_puppi_IDcleaner, topjet_puppi_cleaner, topjet_IDcleaner, topjet_cleaner;
+
+  // Selections
+  //std::unique_ptr<uhh2::AndSelection> metfilters_sel;
+  std::unique_ptr<uhh2::Selection> genflavor_sel;
+  std::unique_ptr<uhh2::Selection> jet1_sel;
+  std::unique_ptr<uhh2::Selection> jet2_sel;
+  std::unique_ptr<uhh2::Selection> met_sel;
+
+  bool isMC, ispuppi;
+  string Sys_PU;
+
+  std::unique_ptr<Hists> lumihists;
+  TString METcollection;
+
+  bool is2016v2, is2016v3, is2017v2, is2018;
+
+};
+
+void NonResonantTTbarPreselectionModule::book_histograms(uhh2::Context& ctx, vector<string> tags){
+  for(const auto & tag : tags){
+    string mytag = tag+"_General";
+    book_HFolder(mytag, new NonResonantTTbarPreselectionHists(ctx,mytag));
+  }
+}
+
+void NonResonantTTbarPreselectionModule::fill_histograms(uhh2::Event& event, string tag){
+    string mytag = tag+"_General";
+    HFolder(mytag)->fill(event);
+}
+
+
+
+NonResonantTTbarPreselectionModule::NonResonantTTbarPreselectionModule(uhh2::Context& ctx){
+
+  for(auto & kv : ctx.get_all()){
+    cout << " " << kv.first << " = " << kv.second << endl;
+  }
+
+  //// CONFIGURATION
+  const TString METcollection = ctx.get("METName");
+  isMC = ctx.get("dataset_type") == "MC";
+  ispuppi = (ctx.get("is_puppi") == "true");
+  is2016v2 = (ctx.get("dataset_version").find("2016v2") != std::string::npos);
+  is2016v3 = (ctx.get("dataset_version").find("2016v3") != std::string::npos);
+  is2017v2 = (ctx.get("dataset_version").find("2017v2") != std::string::npos);
+  is2018 = (ctx.get("dataset_version").find("2018") != std::string::npos);
+  Sys_PU = ctx.get("Sys_PU");
+
+  cout << "Is this running on puppi: " << ispuppi << endl;
+
+
+  ElectronId eleID;  MuonId muID;
+  if(is2017v2 || is2018){
+    eleID = ElectronID_Fall17_tight_noIso;//ToDo: compare cutBased without iso and MVA-based via wp in UHH2
+    muID  = MuonID(Muon::CutBasedIdGlobalHighPt);
+  }
+  if(is2016v2 || is2016v3){
+    //eleID = ElectronID_Summer16_tight_noIso;//ToDo: compare cutBased without iso and MVA-based via wp in UHH2
+    //muID      = MuonID(Muon::Highpt);
+    eleID = ElectronID_Summer16_medium_noIso;
+    muID  = MuonID(Muon::CutBasedIdTight); // see more muonIDs https://github.com/cms-sw/cmssw/blob/master/DataFormats/MuonReco/interface/Muon.h#L201
+  }
+  double electron_pt(50.);
+  double muon_pt(55.);
+  double jet1_pt(50.);
+  double jet2_pt(20.);
+  double MET(50.); // not used
+
+
+
+  // MET filters
+  //metfilters_sel.reset(new uhh2::AndSelection(ctx, "metfilters"));
+  //if(!isMC){
+  //  metfilters_sel->add<TriggerSelection>("1-good-vtx", "Flag_goodVertices");
+  //  metfilters_sel->add<TriggerSelection>("globalTightHalo2016Filter", "Flag_globalTightHalo2016Filter");
+  //  metfilters_sel->add<TriggerSelection>("HBHENoiseFilter", "Flag_HBHENoiseFilter");
+  //  metfilters_sel->add<TriggerSelection>("HBHENoiseIsoFilter", "Flag_HBHENoiseIsoFilter");
+  //  metfilters_sel->add<TriggerSelection>("EcalDeadCellTriggerPrimitiveFilter", "Flag_EcalDeadCellTriggerPrimitiveFilter");
+  //  if(!isMC)  metfilters_sel->add<TriggerSelection>("eeBadScFilter", "Flag_eeBadScFilter");
+  //  metfilters_sel->add<TriggerSelection>("chargedHadronTrackResolutionFilter", "Flag_chargedHadronTrackResolutionFilter");
+  //  metfilters_sel->add<TriggerSelection>("muonBadTrackFilter", "Flag_muonBadTrackFilter");
+  //}
+
+
+  // commented out by Henrik
+  // // GEN Flavor selection [W+jets flavor-splitting]
+  // if(ctx.get("dataset_version").find("WJets") != std::string::npos){
+  //
+  //   if     (ctx.get("dataset_version").find("_B") != std::string::npos) genflavor_sel.reset(new GenFlavorSelection("b"));
+  //   else if(ctx.get("dataset_version").find("_C") != std::string::npos) genflavor_sel.reset(new GenFlavorSelection("c"));
+  //   else if(ctx.get("dataset_version").find("_L") != std::string::npos) genflavor_sel.reset(new GenFlavorSelection("l"));
+  //
+  //   else genflavor_sel.reset(new uhh2::AndSelection(ctx));
+  // }
+  // else genflavor_sel.reset(new uhh2::AndSelection(ctx));
+
+
+  // Cleaning: Mu, Ele, Jets
+  const MuonId muonID(AndId<Muon>(PtEtaCut(muon_pt, 2.4), muID));
+  const ElectronId electronID(AndId<Electron>(PtEtaSCCut(electron_pt, 2.5), eleID));
+  const JetPFID jetID_CHS(JetPFID::WP_TIGHT_CHS);
+  const JetPFID jetID_PUPPI(JetPFID::WP_TIGHT_PUPPI);
+
+  jet_IDcleaner.reset(new JetCleaner(ctx, jetID_PUPPI));
+  jet_cleaner1.reset(new JetCleaner(ctx, 15., 3.0));
+  jet_cleaner2.reset(new JetCleaner(ctx, 30., 2.4));
+  topjet_IDcleaner.reset(new TopJetCleaner(ctx, jetID_CHS, "topjets"));
+  topjet_cleaner.reset(new TopJetCleaner(ctx, TopJetId(PtEtaCut(400., 2.4)), "topjets"));
+  topjet_puppi_IDcleaner.reset(new TopJetCleaner(ctx, jetID_PUPPI, "toppuppijets"));
+  topjet_puppi_cleaner.reset(new TopJetCleaner(ctx, TopJetId(PtEtaCut(400., 2.4)), "toppuppijets"));
+
+
+
+  // common modules
+  common.reset(new CommonModules());
+  common->switch_jetlepcleaner(true);
+  common->disable_pvfilter();
+  common->disable_jetpfidfilter();
+  common->switch_jetPtSorter(true);
+  common->switch_metcorrection(true);
+  common->set_muon_id(muonID);
+  common->set_electron_id(electronID);
+  common->init(ctx, Sys_PU);
+
+  topjetCorr.reset(new TopJetCorrections());
+  topjetCorr->init(ctx);
+
+  toppuppijetCorr.reset(new TopPuppiJetCorrections());
+  toppuppijetCorr->init(ctx);
+
+  //// EVENT SELECTION
+  jet1_sel.reset(new NJetSelection(1, -1, JetId(PtEtaCut(jet1_pt, 2.4))));
+  jet2_sel.reset(new NJetSelection(2, -1, JetId(PtEtaCut(jet2_pt, 2.4))));
+  // met_sel .reset(new METCut  (MET   , uhh2::infinity)); // commented out by Henrik
+
+
+  // Book histograms
+  vector<string> histogram_tags = {"Input", "CommonModules", "Lepton1", "JetID", "JetCleaner1", "JetCleaner2", "TopjetCleaner", "Jet1", "Jet2", "MET"};
+  book_histograms(ctx, histogram_tags);
+
+  lumihists.reset(new LuminosityHists(ctx, "lumi"));
+}
+
+
+bool NonResonantTTbarPreselectionModule::process(uhh2::Event& event){
+
+
+//debug
+//if(event.event==97559444 || event.event==23){
+/*if(event.event!=500494653) return false;
+cout << "Event number = " << event.event << endl;
+
+  cout<<"Getting started... "<<event.event<<endl;
+
+uint jetI= 0;
+for (const Jet & jet: *event.jets) {
+cout << "-- Ak4 jet pt = " << jet.pt() << "	"  << "jet eta = " << jet.eta() << "     "  << "jet phi = " << jet.phi() << "	" << "for jet#" << jetI << endl;
+jetI++;
+}
+uint jetInd = 0;
+for (const TopJet & toppuppijet: *event.toppuppijets) {
+cout << "-- PUPPI Top jet pt = " << toppuppijet.pt()  << "     "  << "jet eta = " << toppuppijet.eta()<< "     "  << "jet phi = " << toppuppijet.phi() << "	"  << "for jet#" << jetInd << endl;
+jetInd++;
+}
+uint chsjetInd = 0;
+for (const TopJet & topjet: *event.topjets) {
+cout << "--- CHS Top jet pt = " << topjet.pt() << "     "  << "jet eta = " << topjet.eta()<< "     "  << "jet phi = " << topjet.phi()  << "	" << "for jet#" << chsjetInd << endl;
+chsjetInd++;
+}
+*/
+
+  //cout<<"Getting started... "<<event.event<<endl;
+  fill_histograms(event, "Input");
+
+  bool commonResult = common->process(event);
+  if (!commonResult) return false;
+  sort_by_pt<Muon>(*event.muons);
+  sort_by_pt<Electron>(*event.electrons);
+  if(ispuppi){
+  toppuppijetCorr->process(event);
+  } else {
+  topjetCorr->process(event);
+  }
+  //cout<<"TopJEC_JLC ... "<<event.event<<endl;
+  //cout<<"Common Modules... "<<event.event<<endl;
+
+  fill_histograms(event, "CommonModules");
+
+  //// MET filters
+  //if(!metfilters_sel->passes(event)) return false;
+  //fill_histograms(event, "Metfilters");
+  ////  cout<<"Met filters ... "<<event.event<<endl;
+
+  // GEN ME quark-flavor selection
+  if(!event.isRealData){
+    if(!genflavor_sel->passes(event)) return false;
+  }
+
+    //cout<<"GEN ME quark-flavor selection ... "<<event.event<<endl;
+
+  const bool pass_lep1 = ((event.muons->size() >= 1) || (event.electrons->size() >= 1));
+  if(!pass_lep1) return false;
+
+  fill_histograms(event, "Lepton1");
+
+  jet_IDcleaner->process(event);
+  fill_histograms(event, "JetID");
+
+  jet_cleaner1->process(event);
+  sort_by_pt<Jet>(*event.jets);
+  fill_histograms(event, "JetCleaner1");
+  //cout<<"JetCleaner1 ... "<<event.event<<endl;
+
+  // Lepton-2Dcut variables
+  for(auto& muo : *event.muons){
+
+    float    dRmin, pTrel;
+    std::tie(dRmin, pTrel) = drmin_pTrel(muo, *event.jets);
+
+    muo.set_tag(Muon::twodcut_dRmin, dRmin);
+    muo.set_tag(Muon::twodcut_pTrel, pTrel);
+  }
+
+  for(auto& ele : *event.electrons){
+
+    float    dRmin, pTrel;
+    std::tie(dRmin, pTrel) = drmin_pTrel(ele, *event.jets);
+
+    ele.set_tag(Electron::twodcut_dRmin, dRmin);
+    ele.set_tag(Electron::twodcut_pTrel, pTrel);
+  }
+
+
+  jet_cleaner2->process(event);
+  sort_by_pt<Jet>(*event.jets);
+  fill_histograms(event, "JetCleaner2");
+    //cout<<"JetCleaner2 ... "<<event.event<<endl;
+
+  topjet_IDcleaner->process(event);
+  topjet_cleaner->process(event);
+  sort_by_pt<TopJet>(*event.topjets);
+
+  topjet_puppi_IDcleaner->process(event);
+  topjet_puppi_cleaner->process(event);
+  sort_by_pt<TopJet>(*event.toppuppijets);
+  fill_histograms(event, "TopjetCleaner");
+    //cout<<"TopjetCleaner ... "<<event.event<<endl;
+
+  // 1st AK4 jet selection
+  const bool pass_jet1 = jet1_sel->passes(event);
+  if(!pass_jet1) return false;
+  fill_histograms(event, "Jet1");
+
+  // 2nd AK4 jet selection
+  const bool pass_jet2 = jet2_sel->passes(event);
+  if(!pass_jet2) return false;
+  fill_histograms(event, "Jet2");
+
+  // MET selection
+  const bool pass_met = met_sel->passes(event);
+  if(!pass_met) return false;
+  fill_histograms(event, "MET");
+
+  return true;
+
+}
+
+UHH2_REGISTER_ANALYSIS_MODULE(NonResonantTTbarPreselectionModule)
